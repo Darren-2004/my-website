6b8197905a0a77a42bc3fd1b45f26effe428664e
Darren <darrentankam@gmail.com>
2025-11-03 16:57:34 +0100

    Initial commit

---
+++ src/Head.jsx
@@ -0,0 +1,18 @@
+
+function Head(){
+    return(
+    <>
+        <nav className="w-500 h-500 bg-blue-500">
+            <div className="flex gap-10 justify-between justify-center p-5 bg-gray-200 text-gray-700 font-bold">
+                <a href="">MENU</a>
+                <a href="">OUR PRODUCTS</a>
+                <a href="">LOGO</a>
+                <a href="">ABOUT US</a>
+                <a href="">CONTACT US</a>
+            </div>
+        </nav>
+    </>
+    );
+}
+
+export default Head;
